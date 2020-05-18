69bc0076eb208648d9d2b5bb17711adcfe59f096
Daniel Soliz Vargas <danielsoliz80@gmail.com>
2020-05-18 19:14:39 -0400

    tabs routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,30 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
+    path: '',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
+  // {
+  //   path: '',
+  //   redirectTo: 'home',
+  //   pathMatch: 'full'
+  // },
+  // {
+  //   path: 'recommendations',
+  //   loadChildren: () => import('./recommendations/recommendations.module').then( m => m.RecommendationsPageModule)
+  // },
+  // {
+  //   path: 'categories',
+  //   loadChildren: () => import('./categories/categories.module').then( m => m.CategoriesPageModule)
+  // },
+  // {
+  //   path: 'search',
+  //   loadChildren: () => import('./search/search.module').then( m => m.SearchPageModule)
+  // },
+  // {
+  //   path: 'profile',
+  //   loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule)
+  // },
 ];
 
 @NgModule({
