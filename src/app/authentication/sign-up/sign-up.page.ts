cdffebe009baa59eb4d6e6b7ed7786662e63b3e0
Daniel Soliz Vargas <danielsoliz80@gmail.com>
2020-05-20 18:59:00 -0400

    sign up page implemented

---
+++ src/app/authentication/sign-up/sign-up.page.ts
@@ -0,0 +1,90 @@
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { AuthenticationService } from "src/app/shared/services/authentication/authentication.service";
+import { ToastController, ModalController } from "@ionic/angular";
+import { SignInPage } from '../sign-in/sign-in.page';
+
+@Component({
+  selector: "app-sign-up",
+  templateUrl: "./sign-up.page.html",
+  styleUrls: ["./sign-up.page.scss"],
+})
+export class SignUpPage implements OnInit {
+  signupForm = new FormGroup({
+    firstName: new FormControl("", [
+      Validators.required,
+      Validators.minLength(2),
+      Validators.pattern("[^' ']+"),
+    ]),
+    lastName: new FormControl("", [
+      Validators.required,
+      Validators.minLength(2),
+      Validators.pattern("[^' ']+"),
+    ]),
+    email: new FormControl("", [
+      Validators.required,
+      Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$"),
+    ]),
+    password: new FormControl("", [
+      Validators.required,
+      Validators.minLength(8),
+    ]),
+  });
+  constructor(
+    private authService: AuthenticationService,
+    private toastController: ToastController,
+    private modalController: ModalController
+  ) {}
+
+  ngOnInit() {}
+
+  onSubmit() {
+    this.authService
+      .signUp(
+        this.email.value,
+        this.password.value,
+        this.firstName.value,
+        this.lastName.value
+      )
+      .then((res: any) => {
+        this.presentToast(
+          "Registro correcto. Verifique su direccion de correo."
+        );
+        this.signupForm.reset();
+      })
+      .catch((error: any) =>
+        this.presentToast("Error al registrarse, intente de nuevo")
+      );
+  }
+
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000,
+    });
+    toast.present();
+  }
+
+  async presentModal() {
+    const modal = await this.modalController.create({
+      component: SignInPage,
+    });
+    return await modal.present();
+  }
+
+  get firstName() {
+    return this.signupForm.get("firstName");
+  }
+
+  get lastName() {
+    return this.signupForm.get("lastName");
+  }
+
+  get email() {
+    return this.signupForm.get("email");
+  }
+
+  get password() {
+    return this.signupForm.get("password");
+  }
+}
