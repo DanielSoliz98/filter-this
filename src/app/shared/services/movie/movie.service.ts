858a9dd3226a2af18e5a56e976da694372a5f523
Daniel Soliz Vargas <danielsoliz80@gmail.com>
2020-05-31 18:13:27 -0400

    service movie implemented

--- src/app/shared/services/movie/movie.service.ts
+++ src/app/shared/services/movie/movie.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from "@angular/core";
 import { HttpHeaders, HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
-import { Movie, MovieModel } from "../../models/movie";
+import { MovieModel } from "../../models/movie";
 import {
   AngularFirestoreDocument,
   AngularFirestore,
+  AngularFirestoreCollection,
 } from "@angular/fire/firestore";
+import { map } from "rxjs/operators";
 
 const httpOptions = {
   headers: new HttpHeaders({
@@ -22,16 +24,38 @@ const url: string =
   providedIn: "root",
 })
 export class MovieService {
+  movieCollection: AngularFirestoreCollection<MovieModel>;
   movieDoc: AngularFirestoreDocument<MovieModel>;
 
-  constructor(private http: HttpClient, private afs: AngularFirestore) {}
+  constructor(private http: HttpClient, private afs: AngularFirestore) {
+    this.movieCollection = this.afs.collection<MovieModel>("movies");
+  }
 
   getMovies(name: string): Observable<any> {
     return this.http.get<any>(url + name, httpOptions);
   }
 
-  getMovie(id: string) {
+  getMovie(id: string): Observable<MovieModel> {
     this.movieDoc = this.afs.doc<MovieModel>(`movies/${id}`);
     return this.movieDoc.valueChanges();
+  }
+
+  getMoviesSaved(): Observable<MovieModel[]> {
+    return this.movieCollection.snapshotChanges().pipe(
+      map((actions) =>
+        actions.map((a) => {
+          const data = a.payload.doc.data() as MovieModel;
+          const id = a.payload.doc.id;
+          return { id, ...data };
+        })
+      )
+    );
+  }
+
+  addMovie(data: MovieModel): Promise<void> {
+    const movieRef: AngularFirestoreDocument<any> = this.afs.doc(
+      `movies/${data.id}`
+    );
+    return movieRef.set(data);
   }
 }
