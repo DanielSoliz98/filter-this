016dc0df8ea060f645da9752ae03af0a44dee4c5
Daniel Soliz Vargas <danielsoliz80@gmail.com>
2020-06-26 22:36:05 -0400

    restaurant detail component added

--- src/app/profile/my-recommendations/my-recommendations.component.ts
+++ src/app/profile/my-recommendations/my-recommendations.component.ts
@@ -18,11 +18,12 @@ import { RestaurantService } from "src/app/shared/services/restaurant/restaurant
 import { UserService } from "src/app/shared/services/user/user.service";
 import { ModalController } from "@ionic/angular";
 import { MovieDetailComponent } from "src/app/recommendations/categories/movie/movie-detail/movie-detail.component";
-import { MusicDetailComponent } from 'src/app/recommendations/categories/music/music-detail/music-detail.component';
-import { SerieDetailComponent } from 'src/app/recommendations/categories/tv-series/serie-detail/serie-detail.component';
-import { BookDetailComponent } from 'src/app/recommendations/categories/book/book-detail/book-detail.component';
-import { GameDetailComponent } from 'src/app/recommendations/categories/games/game-detail/game-detail.component';
-import { ProductDetailComponent } from 'src/app/shared/components/product-detail/product-detail.component';
+import { MusicDetailComponent } from "src/app/recommendations/categories/music/music-detail/music-detail.component";
+import { SerieDetailComponent } from "src/app/recommendations/categories/tv-series/serie-detail/serie-detail.component";
+import { BookDetailComponent } from "src/app/recommendations/categories/book/book-detail/book-detail.component";
+import { GameDetailComponent } from "src/app/recommendations/categories/games/game-detail/game-detail.component";
+import { ProductDetailComponent } from "src/app/shared/components/product-detail/product-detail.component";
+import { RestaurantDetailComponent } from "src/app/shared/components/restaurant-detail/restaurant-detail.component";
 
 @Component({
   selector: "app-my-recommendations",
@@ -186,6 +187,17 @@ export class MyRecommendationsComponent implements OnInit {
       component: ProductDetailComponent,
       componentProps: {
         product: product,
+        showComments: true,
+      },
+    });
+    return await modal.present();
+  }
+
+  async openRestaurant(restaurant: Restaurant) {
+    const modal = await this.modalCtrl.create({
+      component: RestaurantDetailComponent,
+      componentProps: {
+        restaurant: restaurant,
         showComments: true,
       },
     });
--- src/app/shared/components/restaurant-detail/restaurant-detail.component.ts
+++ src/app/shared/components/restaurant-detail/restaurant-detail.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ModalController, ToastController } from '@ionic/angular';
+import { AuthenticationService } from '../../services/authentication/authentication.service';
+import { UserService } from '../../services/user/user.service';
+import { Restaurant } from '../../models/restaurant';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-restaurant-detail',
@@ -6,9 +11,70 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./restaurant-detail.component.scss'],
 })
 export class RestaurantDetailComponent implements OnInit {
-
-  constructor() { }
+  @Input() restaurant: Restaurant;
+  @Input() showComments: boolean;
+  user: User;
+  saved: boolean;
+  myRecommendation: boolean;
+  sliderConfig = {
+    slidesPerView: 1,
+  };
+  constructor(
+    private modalController: ModalController,
+    private toastController: ToastController,
+    private authService: AuthenticationService,
+    private userService: UserService
+  ) {}
 
   ngOnInit() {}
 
+  ionViewWillEnter() {
+    if (this.restaurant && this.showComments) {
+      this.myRecommendation =
+        this.restaurant.user_uid === this.authService.userData.uid ? true : false;
+      this.userService.getUser(this.restaurant.user_uid).subscribe((dataUser) => {
+        this.user = dataUser;
+      });
+    }
+
+    this.userService
+      .getMyCollection(this.authService.userData.uid)
+      .subscribe((data) => {
+        let inCollection = data.restaurants.find((restaurant) => restaurant === this.restaurant.id);
+        if (!this.myRecommendation) {
+          this.saved = inCollection ? true : false;
+        } else {
+          this.saved = false;
+        }
+      });
+  }
+
+  saveToMyCollection() {
+    this.userService
+      .getMyCollection(this.authService.userData.uid)
+      .subscribe((data) => {
+        let collection = data;
+        collection.restaurants.push(this.restaurant.id);
+        this.userService
+          .updateMyCollection(this.authService.userData.uid, collection)
+          .then(() => {
+            this.dismiss();
+            this.presentToast("Recomendacion guardada");
+          });
+      });
+  }
+
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000,
+    });
+    toast.present();
+  }
+
+  dismiss() {
+    this.modalController.dismiss({
+      dismissed: true,
+    });
+  }
 }
